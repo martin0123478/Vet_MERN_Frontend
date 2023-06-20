e56fd32db4e8135d0557c0ca0cb734999a9cd7c0
martin0123478 <martin_agui97@outlook.es>
2023-06-20 16:05:11 -0600

    feat:add design login, register, forget password

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Login = () => {
   return (
     <>
-        <h2>Login</h2>
+      
+          <div>
+            <h1 className='text-indigo-600 font-black text-6xl'>Inicia Sesión y Administra tus <span className='text-black'>Pacientes</span> </h1>
+          </div>
+          <div className='mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white'>
+            <form>
+              <div className='my-5'>
+              <label className='uppercase text-gray-600 block text-xl font-bold' >Email</label>
+              <input type="email" placeholder='Email de registro' className='w-full border p-3 mt-3 bg-50 rounded-xl' />
+              </div>
+              <div className='my-5'>
+              <label className='uppercase text-gray-600 block text-xl font-bold' >Password</label>
+              <input type="password" placeholder='Password' className='w-full border p-3 mt-3 bg-50 rounded-xl' />
+              </div>
+              <input type="submit" value='iniciar sesión' className='bg-indigo-700 w-full py-3  px-10 rounded-xl text-white uppercase font-bold mt-5 hover:cursor-pointer hover:bg-indigo-800 md:w-auto '/>
+            </form>
+            <nav className='mt-10 lg:flex lg:justify-between'>
+              <Link 
+              className='block text-center my-5 text-gray-500'
+              to="/registrar">¿No tienes una cuenta? Registrate</Link>
+              <Link
+              className='block text-center my-5 text-gray-500'
+              to="/olvide-password">Olvide mi password</Link>
+            </nav>
+          </div>
+        
     </>
   )
 }
