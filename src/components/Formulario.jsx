cb06c1359f40c841018e2b66009bfc252c25d99f
martin0123478 <martin_agui97@outlook.es>
2023-07-11 17:31:33 -0600

    feat:validate form

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,36 +1,62 @@
-import React from 'react'
-
+import { useState } from "react"
+import Alerta from "./Alerta"
 const Formulario = () => {
+    const [nombre, setNombre] = useState('')
+    const [propietario, setPropietario] = useState('')
+
+    const [email, setEmail] = useState('')
+    const [fecha, setFecha] = useState(Date.now())
+    const [sintomas, setSintomas] = useState('')
+    const [alerta, setAlerta] = useState({})
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        if ([nombre, propietario, email, fecha, sintomas].includes('')) {
+            setAlerta({
+                msg: 'Todos los campos son obligatorios',
+                error: true
+            })
+            return
+        }
+    }
+    const { msg } = alerta
     return (
         <>
             <p>
                 Añade a tus pacientes y {''}
                 <span className='text-indigo-600 font-bold'>Adminsitralos</span>
             </p>
-            <form className='bg-white py-10 px-5 mt-10 mb-10 lg:mb-10 shadow-md rounded-md'>
+            {msg && <Alerta alerta={alerta} />}
+            <form className='bg-white py-10 px-5 mt-10 mb-10 lg:mb-10 shadow-md rounded-md'
+                onSubmit={handleSubmit}>
                 <div className='mb-5 '>
-                    <label className='text-gray-700 uppercase font-bold' htmlFor='mascota'>Nombre Mascotas</label>
-                    <input className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md' id='mascota' placeholder='Nombre de la mascota' type="text" />
+                    <label className='text-gray-700 uppercase font-bold' htmlFor='nombre'>Nombre Mascotas</label>
+                    <input className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md' id='nombre' placeholder='Nombre de la mascota' type="text"
+                        value={nombre} onChange={e => setNombre(e.target.value)} />
                 </div>
 
                 <div className='mb-5'>
                     <label className='text-gray-700 uppercase font-bold' htmlFor='email'>Nombre del Propietario</label>
-                    <input className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md' id='email' placeholder='Nombre del Propietario' type="text" />
+                    <input className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md' id='email' placeholder='Nombre del Propietario' type="text"
+                        value={propietario} onChange={e => setPropietario(e.target.value)} />
                 </div>
 
                 <div className='mb-5'>
                     <label className='text-gray-700 uppercase font-bold' htmlFor='email'>Email Propietario</label>
-                    <input className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md' id='email' placeholder='Email del Propietario' type="email" />
+                    <input className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md' id='email' placeholder='Email del Propietario' type="email"
+                        value={email} onChange={e => setEmail(e.target.value)} />
                 </div>
 
                 <div className='mb-5'>
                     <label className='text-gray-700 uppercase font-bold' htmlFor='fecha'>Fecha alta </label>
-                    <input className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md' id='fecha' type="date" />
+                    <input className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md' id='fecha' type="date"
+                        value={fecha} onChange={e => setFecha(e.target.value)} />
                 </div>
 
                 <div className='mb-5'>
                     <label className='text-gray-700 uppercase font-bold' htmlFor='sintomas'>Sintomas </label>
-                    <textarea className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md' id='sintomas' placeholder='Describe los sintomas' />
+                    <textarea className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md' id='sintomas' placeholder='Describe los sintomas'
+                        value={sintomas} onChange={e => setSintomas(e.target.value)} />
                 </div>
 
                 <input className='bg-indigo-600 w-full p-3 text-white uppercase font-bold 
--- src/pages/AdministradorPacientes.jsx
+++ src/pages/AdministradorPacientes.jsx
@@ -1,10 +1,13 @@
+import { useState } from "react"
 import Formulario from "../components/Formulario"
 import ListadoPacientes from "../components/ListadoPacientes"
 
 function AdministradorPacientes() {
+    const [mostratFormulario, setMostratFormulario] = useState(false)
     return (
         <div className="flex flex-col md:flex-row">
-            <div className="md:w-1/2 lg:w-2/5">
+            <button type="button" className="bg-indigo-600 text-white font-bold uppercase mx-10 p-3 rounded-md md:hidden" onClick={() => setMostratFormulario(!mostratFormulario)}>Mosrar Formulario</button>
+            <div className={`${mostratFormulario ? 'block' : 'hidden'} md:block md:w-1/2 lg:w-2/5`}>
                 <Formulario />
             </div>
             <div className="md:w-1/2 lg:w-3/5">
